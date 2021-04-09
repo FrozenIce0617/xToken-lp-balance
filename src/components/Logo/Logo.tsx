e893fa1b92a5abfd428832fdb9612890f9ea4a43
FrozenIce0617 <prosdev087@gmail.com>
2021-04-09 15:18:38 +0100

    Add Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+import LogoImg from '../../assets/images/logo.png';
+
+const useStyles = makeStyles({
+  logo: {
+    borderRadius: 24,
+    width: 48,
+    height: 48,
+  },
+});
+
+const Logo = (props) => {
+  const classes = useStyles();
+
+  return <img className={classes.logo} src={LogoImg} alt="Logo" {...props} />;
+};
+
+export default Logo;
