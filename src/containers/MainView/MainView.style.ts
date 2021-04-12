1944c3bac3db7c26d7d53c9fed29a4c1d2b65cd3
FrozenIce0617 <prosdev087@gmail.com>
2021-04-12 15:22:08 +0100

    Update the MainView

---
+++ src/containers/MainView/MainView.style.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  mainWrapper: {
+    marginTop: theme.spacing(4),
+  },
+  table: {
+    width: '100%',
+  },
+}));
+
+export default useStyles;
--- src/containers/MainView/MainView.tsx
+++ src/containers/MainView/MainView.tsx
@@ -1,5 +1,31 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+import useStyles from './MainView.style';
+
 const MainView: React.FC = () => {
-  return <span>Main View</span>;
+  const classes = useStyles();
+
+  return (
+    <TableContainer className={classes.mainWrapper} component={Paper}>
+      <Table className={classes.table} aria-label="caption table">
+        <caption>Token List</caption>
+        <TableHead>
+          <TableRow>
+            <TableCell>Token Name</TableCell>
+            <TableCell align="right">Balance</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody></TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
 export default MainView;
